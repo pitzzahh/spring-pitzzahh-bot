5ec9e8ea843dee3808520726442a044ff0b882b0
pitzzahh <pitzzahh@araopj.tech>
2023-02-22 08:51:59 +0800

    add: more config

--- src/main/java/tech/araopj/springpitzzahhbot/config/moderation/ModerationConfig.java
+++ src/main/java/tech/araopj/springpitzzahhbot/config/moderation/ModerationConfig.java
@@ -1,12 +1,21 @@
 package tech.araopj.springpitzzahhbot.config.moderation;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.*;
 
+@Getter
 @Configuration
 public class ModerationConfig {
+
+    @Value("${bot.moderation.reply-deletion-delay-in-minutes}")
+    public int replyDeletionDelayInMinutes;
+
+    @Value("${bot.moderation.message-deletion-delay-in-seconds}")
+    public int messageDeletionDelayInSeconds;
 
     @Bean
     public Map<String, Integer> violations() {

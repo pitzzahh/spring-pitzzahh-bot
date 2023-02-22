d482f252d3d79b53e88bc9390196fcea23936c7d
pitzzahh <pitzzahh@araopj.tech>
2023-02-22 09:22:17 +0800

    refactor: from record to class

--- src/main/java/tech/araopj/springpitzzahhbot/config/service/DiscordBotConfigService.java
+++ src/main/java/tech/araopj/springpitzzahhbot/config/service/DiscordBotConfigService.java
@@ -1,10 +1,19 @@
 package tech.araopj.springpitzzahhbot.config.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import tech.araopj.springpitzzahhbot.config.DiscordBotConfig;
 import org.springframework.stereotype.Service;
 
 @Service
-public record DiscordBotConfigService(DiscordBotConfig discordBotConfig) {
+public final class DiscordBotConfigService {
+
+    private final DiscordBotConfig discordBotConfig;
+
+    @Autowired
+    public DiscordBotConfigService(DiscordBotConfig discordBotConfig) {
+        this.discordBotConfig = discordBotConfig;
+    }
+
     public String getToken() {
         return discordBotConfig.getToken();
     }
